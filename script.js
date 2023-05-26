8022737bc7120fc532bbe426242f021ff7ee29a3
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-26 12:12:20 +0530

    added addTask function

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+function addTask()
+{
+    if(inputBox.value==="")
+    {
+        alert("Please Write Something");
+    }
+    else
+    {
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+        let editIcon = document.createElement("span2");
+        editIcon.innerHTML = "&#8942;";
+        li.appendChild(editIcon);
+    }
+    inputBox.value="";
+}
+
