70bcac7524e29f8c6c2903b4704b4a8036c3a09e
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-26 12:13:57 +0530

    added eventListner after click

--- script.js
+++ script.js
@@ -22,6 +22,34 @@ function addTask()
     saveData()
 }
 
+listContainer.addEventListener("click",function(e)
+{
+    if(e.target.tagName==="LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if(e.target.tagName==="SPAN")
+    {
+        e.target.parentElement.remove();
+        saveData()
+    }
+    else if(e.target.tagName==="SPAN2")
+    {
+        console.log(e.target.parentElement.innerText)
+        let taskString=e.target.parentElement.innerText;
+        let newString="";
+        for(let i=0; i<taskString.length-3; i++)
+        {
+            newString+=taskString[i];
+        }
+        inputBox.value=newString;
+        e.target.parentElement.remove();
+        saveData()
+    }
+
+},false)
+
 function saveData()
 {
     localStorage.setItem("data", listContainer.innerHTML);
